cce651a531af12cb2ab60c16d74cee71773f17cb
Votre nom ou pseudo <Votre@email.com>
2018-05-03 11:48:27 +0200

    Bienvenue dans le nouveau repertoire

---
+++ src/main/java/com/isep/cardis/projet_fiches_module_A2/ProjetFichesModuleA2Application.java
@@ -0,0 +1,32 @@
+package com.isep.cardis.projet_fiches_module_A2;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProjetFichesModuleA2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFichesModuleA2Application.class, args);
+	}
+	
+	@Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+}
---
+++ src/test/java/com/isep/cardis/projet_fiches_module_A2/ProjetFichesModuleA2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.isep.cardis.projet_fiches_module_A2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetFichesModuleA2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
